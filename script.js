ba5a22c681b81e83d4cc6e033113e68ca1aa5e90
John Bonczek <94026273+Johnny-I5-Back@users.noreply.github.com>
2022-06-20 04:24:33 +0000

    finished tutorial

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamJvbmN6ZWsxMjMiLCJhIjoiY2w0bTd3aWd1MDRxeTNkcGVvb2FhenplayJ9._l_QYxejSorApxXduCv2zw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-71.060982, 42.35725],
+zoom: 18,
+});
